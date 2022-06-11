909e6a36ccd0366e98bea308e78194b36e1b9130
hamza ameur <hameur@student.1337.ma>
2022-06-11 16:33:54 +0100

    change sort_big fct

--- push_swap.c
+++ push_swap.c
@@ -36,19 +36,22 @@ int main(int ac, char **av)
 	(void)av;
 	s_aa = NULL;
 	s_aa = check_str(av[1], s_aa);
-	foct(&s_aa, &s_bb);
-	// rank_stacks(&s_aa);
-	// t_stack *temp = s_aa;
-	// while(temp != NULL)
-	// {
-	// 	printf("|%ld| == %d\n |\n",temp->x, temp->index);
-	// 	temp = temp->next;
-	// }
-	// printf("\n\n");
-	// ft_fct(&s_aa, &s_bb);
-	// while(s_bb != NULL)
-	// {
-	// 	printf("|%ld| == %d\n |\n",s_bb->x, s_bb->index);
-	// 	s_bb = s_bb->next;
-	// }
+	rank_stacks(&s_aa);
+	ft_fct(&s_aa, &s_bb);
+	
+	
+	t_stack *temp = s_aa;
+	while(temp != NULL)
+	{
+	printf("|%ld| == %d\n |\n",temp->x, temp->index);
+	temp = temp->next;
+	}
+	printf("\n\n");
+	while(s_bb != NULL)
+	{
+	 	printf("|%ld| == %d\n |\n",s_bb->x, s_bb->index);
+	 	s_bb = s_bb->next;
+	}
+
+	
 }
--- sort/sort_big.c
+++ sort/sort_big.c
@@ -15,42 +15,90 @@
 
 t_stack **zero(t_stack **s_b)
 {
-	t_stack *zero;
+	t_stack *ptr;
 	
-	zero = (t_stack *)malloc(sizeof(t_stack));
-	zero->index = 0;
-	zero->next = NULL;
-	zero->x = 0;
-	*s_b = zero;
+	ptr = (t_stack *)malloc(sizeof(t_stack));
+	ptr = ft_lstnew(0);
+	ptr->index = 0;
+	ft_lstadd_back(&ptr, ft_lstnew(1));
+	ptr->next->index = 1;
+	ptr->next->next = NULL;
+	*s_b = ptr;
 	return (s_b);
+}
+
+void push_down(t_stack **s_b, t_stack **s_a, t_stack *midle)
+{
+	int i;
+	int j;
+	t_stack *temp;
+
+	i = 0;
+	j = 0;
+	temp = midle;
+	if ((*s_a)->index == 1)
+	{
+		midle->x = (*s_a)->x;
+		ft_lstdelfirst(*s_a);
+		return ;
+	}
+	while (temp != NULL)
+	{
+		if ((*s_a)->index < temp->index)
+			i++;
+		temp = temp->next;
+	}
+	j = i + 1;
+	while (i-- >= 0)
+		r_rotate_b(s_b);
+	push_b(s_b, s_a);
+	while (j-- >= 0)
+		rotate_b(s_b);
+}
+
+void push_up(t_stack **s_b, t_stack **s_a, t_stack *first)
+{
+	t_stack *temp;
+	int i;
+
+	i = 0;
+	temp = *s_b;
+	if ((*s_a)->index == 0)
+	{
+		first->x = (*s_a)->x;
+		ft_lstdelfirst(*s_a);
+		return ;
+	}
+	while ((*s_a)->index < (*s_b)->index)
+	{
+		printf("s_a == %d\n", temp->index);
+		if (temp->index == 0)
+			break;
+		rotate_b(s_b);
+		i++;
+		temp = *s_b;
+		sleep(1);
+	}
+	push_b(s_b, s_a);
+	while (i-- >= 0)
+		r_rotate_b(s_b);
 }
 
 void	ft_fct(t_stack **s_a, t_stack **s_b)
 {
 	t_stack *first;
 	t_stack *midle;
-	t_stack *ptr = *s_a;
-	int i = 0;
-	int j = 0;
 	
 	s_b = zero(s_b);
-	midle = *s_b;
 	first = *s_b;
-	while(ptr != NULL)
+	midle = (*s_b)->next;
+	while (*s_a != NULL)
 	{
-		if(ptr->index % 2 == 0)
-		{
-			while (ptr->index < first->index)
-			{
-				rotate_b(s_b);
-				first = *s_b;
-				i++;
-			}
-			push_b(s_b, s_a);
-			while (i-- >= 0)
-				r_rotate_b(s_b);
-			first = *s_b;
-		}
+		if ((*s_a)->index % 2 == 0)	
+			push_up(s_b, s_a, first);
+		else if ((*s_a)->index % 2 == 1)
+			push_down(s_b, s_a, midle);
 	}
+	
 	
 }
